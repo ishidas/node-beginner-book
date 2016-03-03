8dbd590cff24bac6f89e5f9ba2cfad815526b80c
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-03 12:57:32 -0800

    refactored to include index.js and server from there

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require('http');
 
-function onRequest(request, response){
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write('Hello world!');
-  response.end();
-}
+function start (){
+  function onRequest(request, response){
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write('hello world!');
+    response.end();
+  }
+
   http.createServer(onRequest).listen(8888);
+  console.log('Server has started.');
+};
+
+exports.start = start;
