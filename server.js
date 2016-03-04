c162f0abadcc6720e354cef4e455530bf63f5002
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-03 21:16:35 -0800

    route request

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require('http');
+var url = require('url');
+var router = require('./router');
 
 function start (){
   function onRequest(request, response){
+    var pathname = url.parse(request.url).pathname;
+    console.log('Requeest for ' + pathname + ' received.');
+
+    router.route(pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write('hello world!');
     response.end();
