84e2f4727417b438dce9d562f46f96afd731956b
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-03 22:11:39 -0800

    routing and printing inconsole correctly

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var http = require('http');
 var url = require('url');
-var router = require('./router');
 
-function start (){
+
+function start (route, handle){
   function onRequest(request, response){
     var pathname = url.parse(request.url).pathname;
-    console.log('Requeest for ' + pathname + ' received.');
+    console.log('Request for ' + pathname + ' received.');
 
-    router.route(pathname);
+    route(handle, pathname);
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write('hello world!');
