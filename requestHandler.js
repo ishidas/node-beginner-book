c7bac11ca8180cf1e870c0a44a1c2ff01f705e39
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-03 22:22:11 -0800

    route showing hello star and upload on browser

--- requestHandler.js
+++ requestHandler.js
@@ -1,9 +1,11 @@
 function start (){
   console.log('Request Handler "start" was called.');
+  return 'Hello Start'
 };
 
 function upload (){
   console.log('Request Handler "upload" was called.');
+  return 'Hello Upload'
 };
 
 exports.start = start;
